fe2b94cbc2b5f3d2137a358c77a4dbf32a4e8f21
ouyangliqi <ouyangliqi16@gmail.com>
2020-12-15 18:08:26 +0800

    outdict-doing

--- run.sh
+++ run.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
-MODEL_PATH_A=checkpoints/pretrain-model.torch
-MODEL_PATH_B=checkpoints/finetune-model.torch
+MODEL_PATH_A=checkpoints/nosiy_data_1/pretrain-model.torch
+MODEL_PATH_B=checkpoints/nosiy_data_1/finetune-model.torch
 DATASET=datasets
-#CUDA_VISIBLE_DEVICES=2 python bin/main.py \
-#    --model crf \
-#    --batch_size 32 \
-#    --learning_rate 2e-5 \
-#    --do_train \
-#    --do_test \
-#    --trainset  $DATASET/train_data_raw.json \
-#    --evalset $DATASET/test_data_raw.json \
-#    --testset $DATASET/test_data_raw.json \
-#    --bert_load_mode from_pretrained \
-#    --model_save_path $MODEL_PATH_A
-#
-#CUDA_VISIBLE_DEVICES=2 python bin/main.py \
-#    --model crf\
-#    --batch_size 32 \
-#    --learning_rate 2e-5 \
-#    --do_train \
-#    --do_test \
-#    --trainset  $DATASET/appen/train_data_raw.json \
-#    --evalset $DATASET/appen/val_data_raw.json \
-#    --testset $DATASET/appen/test_data_raw.json \
-#    --bert_load_mode bert_only \
-#    --model_load_path $MODEL_PATH_A \
-#    --model_save_path $MODEL_PATH_B
-
-# baseline
 CUDA_VISIBLE_DEVICES=2 python bin/main.py \
     --model crf \
+    --batch_size 32 \
+    --learning_rate 2e-5 \
+    --do_train \
+    --do_test \
+    --trainset  $DATASET/train_data_raw.json \
+    --evalset $DATASET/test_data_raw.json \
+    --testset $DATASET/test_data_raw.json \
+    --bert_load_mode from_pretrained \
+    --model_save_path $MODEL_PATH_A
+
+CUDA_VISIBLE_DEVICES=2 python bin/main.py \
+    --model crf\
     --batch_size 32 \
     --learning_rate 2e-5 \
     --do_train \
@@ -38,5 +24,20 @@ CUDA_VISIBLE_DEVICES=2 python bin/main.py \
     --trainset  $DATASET/appen_not_filter/train_data_raw.json \
     --evalset $DATASET/appen_not_filter/val_data_raw.json \
     --testset $DATASET/appen_not_filter/test_data_raw.json \
-    --bert_load_mode from_pretrained \
-    --model_save_path checkpoints/baseline.torch
+    --bert_load_mode bert_only \
+    --model_load_path $MODEL_PATH_A \
+    --model_save_path $MODEL_PATH_B
+
+# baseline
+#CUDA_VISIBLE_DEVICES=2 python bin/main.py \
+#    --model crf \
+#    --batch_size 32 \
+#    --learning_rate 2e-5 \
+#    --do_train \
+#    --do_test \
+#    --do_outdict \
+#    --trainset  $DATASET/appen_not_filter/train_data_raw.json \
+#    --evalset $DATASET/appen_not_filter/val_data_raw.json \
+#    --testset $DATASET/appen_not_filter/test_data_raw.json \
+#    --bert_load_mode from_pretrained \
+#    --model_save_path checkpoints/baseline.torch
--- test/test_model_state.py
+++ test/test_model_state.py
@@ -2,21 +2,30 @@
 # @Time    : 12/14/2020 10:03 AM
 # @Author  : Chloe Ouyang
 # @FileName: test_model_state.py
+import sys
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 import torch
 from models import BERTSoftmax, BERTCRF
 from transformers import BertConfig
 
 
 def test_model_state():
-    pretrain_model_path = "checkpoints/pretrain-model.torch"
+    pretrain_model_path = "checkpoints/nosiy_data_1/pretrain-model.torch"
     model_name = "bert-base-chinese"
 
     config = BertConfig.from_pretrained(model_name)
     pretrain_model_state = torch.load(pretrain_model_path)
-    finetune_model_state = BERTCRF.from_pretrained(model_name, config=config, num_labels=33).state_dict()
+    finetune_model_state = BERTCRF.from_pretrained(model_name, config=config, num_labels=33).to("cuda").state_dict()
     assert pretrain_model_state.keys() == finetune_model_state.keys()
     for key in pretrain_model_state.keys():
         if key.startswith("bert."):
             assert pretrain_model_state[key] == finetune_model_state[key]
         else:
             assert pretrain_model_state[key] != finetune_model_state[key]
+
+
+if __name__ == '__main__':
+    test_model_state()

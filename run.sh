3fb21637e52dc9b41c01b3e9ca8afe72085da306
ouyangliqi <ouyangliqi16@gmail.com>
2020-12-22 14:34:40 +0800

    outdict-seqeval-ongoing

--- run.sh
+++ run.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-MODEL_PATH_A=checkpoints/noisy_data_1/pretrain-model.torch
-MODEL_PATH_B=checkpoints/noisy_data_1/finetune-model.torch
+MODEL_PATH_A=checkpoints/noisy_data_3_seed12345/pretrain-model.torch
+MODEL_PATH_B=checkpoints/noisy_data_3_seed12345/finetune-model.torch
 DATASET=datasets
 #CUDA_VISIBLE_DEVICES=2 python bin/main.py \
 #    --model crf \
 #    --batch_size 32 \
 #    --learning_rate 2e-5 \
+#    --epochs 5 \
 #    --do_train \
 #    --do_test \
-#    --trainset  $DATASET/noisy_data_2/train_data_raw.json \
-#    --evalset $DATASET/noisy_data_2/test_data_raw.json \
-#    --testset $DATASET/noisy_data_2/test_data_raw.json \
+#    --trainset  $DATASET/noisy_data_3/train_data_raw.json \
+#    --evalset $DATASET/noisy_data_3/test_data_raw.json \
+#    --testset $DATASET/noisy_data_3/test_data_raw.json \
 #    --bert_load_mode from_pretrained \
 #    --model_save_path $MODEL_PATH_A
 #
 #CUDA_VISIBLE_DEVICES=2 python bin/main.py \
-#    --model crf\
+#    --model crf \
 #    --batch_size 32 \
 #    --learning_rate 2e-5 \
+#    --epochs 5 \
 #    --do_train \
 #    --do_test \
 #    --do_outdict \
@@ -33,11 +35,12 @@ CUDA_VISIBLE_DEVICES=2 python bin/main.py \
     --model crf \
     --batch_size 32 \
     --learning_rate 2e-5 \
+    --epochs 5 \
     --do_train \
     --do_test \
     --do_outdict \
-    --trainset  $DATASET/appen_filter_others/train_data_raw.json \
-    --evalset $DATASET/appen_filter_others/val_data_raw.json \
-    --testset $DATASET/appen_not_filter/test_data_raw.json \
+    --trainset  $DATASET/appen_not_filter_equal_sampling/train_data_raw.json \
+    --evalset $DATASET/appen_not_filter_equal_sampling/val_data_raw.json \
+    --testset $DATASET/appen_not_filter_equal_sampling/test_data_raw.json \
     --bert_load_mode from_pretrained \
-    --model_save_path checkpoints/baseline_filter_others_seed1.torch
+    --model_save_path checkpoints/baseline_appen_not_filter_equal_sampling_seed12345.torch
